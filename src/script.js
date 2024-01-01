45282803b30c65bb9ec28a2a8815b9a73077a225
ritik02122002 <ritikgoyal4019@gmail.com>
2024-01-01 20:59:43 +0530

    modified script.js

--- src/script.js
+++ src/script.js
@@ -106,7 +106,7 @@ const routerElement=createBrowserRouter(
    
     
     ]);
-root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(<RouterProvider router={routerElement}/>);
 
